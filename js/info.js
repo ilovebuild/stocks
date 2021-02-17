89ede1e223af67baadf8ac3e2965e01750a27804
ilovebuild <ilovebuild@gmail.com>
2021-02-18 00:30:34 +0900

    1

--- js/info.js
+++ js/info.js
@@ -24,13 +24,6 @@ function buildTable(mkt) {
     var data = localStorage.getItem(mkt);
     if(  data ){
         stocks_data[mkt] = JSON.parse(data);
-
-        for(var i=0; i<stocks_data[mkt].length ;++i)
-        {
-            var name = stocks_data[mkt][i][0];
-            var code = stocks_data[mkt][i][1].toString().lpad(6,'0');
-            stocks_data[mkt][i][0] = '<a href="http://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A'+code+'&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN=" target="_blank">'+name+'</a>'
-        }
     }
 
     var datas = stocks_data[mkt];
@@ -40,12 +33,21 @@ function buildTable(mkt) {
     $(document).ready(function() {
         datatable = $('#dataTable').DataTable(
             {
-                paging:false,
-                langthChange:false,
-                filter:false,
-                ordering:false,
+                //paging:false,
+                //langthChange:false,
+                //filter:false,
+                //ordering:false,
+                scrollY: true,
+                scrollX: true,
+                order: [[10, 'desc']],
                 data:datas,
-            }
+                lengthMenu: [
+                    [ 25, 50, 100 ],
+                    [ '25 rows', '50 rows', '100 rows' ]
+                ],
+
+            },
+            
         );
     });
 
@@ -63,6 +65,7 @@ async function submitformGET(mkt) {
         }
     }
     var url = 'https://b1thhkvj25.execute-api.ap-northeast-2.amazonaws.com/stocks/datalist?MKT='+mkt;
+    //var url = 'http://localhost:8001?MKT='+mkt;
     xhr.open('GET', url,true);
     xhr.setRequestHeader('Content-Type', 'text/plain');
     xhr.send(null);
@@ -86,6 +89,7 @@ async function submitformPOST(mkt) {
         }
     }
     var url = 'https://b1thhkvj25.execute-api.ap-northeast-2.amazonaws.com/stocks/datalist';
+    //var url = 'http://localhost:8001?MKT='+mkt;
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(jsonData);
